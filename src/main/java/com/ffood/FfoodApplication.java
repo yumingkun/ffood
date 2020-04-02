da81fde4c055c0862b820485768ad6e4c0f25010
ymk <1934495742@qq.com>
2020-04-02 22:43:16 +0800

    Initial commit

---
+++ src/main/java/com/ffood/FfoodApplication.java
@@ -0,0 +1,13 @@
+package com.ffood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ffood/FfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
